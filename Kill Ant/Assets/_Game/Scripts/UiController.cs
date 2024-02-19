51bb6f81275a9a033e8bed3f71b5d05977e81a82
Albany Timbó Mesquita <4lb4ny@gmail.com>
2024-02-19 17:33:50 -0300

    Pause do game

--- Kill Ant/Assets/_Game/Scripts/UiController.cs
+++ Kill Ant/Assets/_Game/Scripts/UiController.cs
@@ -8,8 +8,11 @@ public class UiController : MonoBehaviour
 {
     public TMP_Text txtScore;
     public Image[] imageLifes;
+    public GameObject panelGame, panelPause;
     void Start()
     {
+        panelGame.gameObject.SetActive(true);
+        panelPause.gameObject.SetActive(false);
         
     }
 
@@ -21,4 +24,20 @@ public class UiController : MonoBehaviour
     public void UpdateScore(int score){
         txtScore.text = score.ToString();
     }
+    public void ButtonPause(){
+        Time.timeScale = 0f;
+        panelGame.gameObject.SetActive(false);
+        panelPause.gameObject.SetActive(true);
+    }
+    public void ButtonResume(){
+        Time.timeScale = 1f;
+        panelGame.gameObject.SetActive(true);
+        panelPause.gameObject.SetActive(false);
+    }
+    public void ButtonBackMainMenu(){
+        Time.timeScale = 1f;
+        panelGame.gameObject.SetActive(false);
+        panelPause.gameObject.SetActive(false);
+    }
+
 }
